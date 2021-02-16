3876d4807abe98455a290b3ca450dfed0577de53
John Jewell Diño <johnjewelldino@Rexons-MacBook-Pro-2.local>
2021-02-16 23:41:57 +0800

    api-config: Update ContactController, Add save and delete methods on ContactRepository, Add lname and fname for ContactType

--- src/ApiBundle/Controller/V1/ContactController.php
+++ src/ApiBundle/Controller/V1/ContactController.php
@@ -10,6 +10,8 @@ namespace App\ApiBundle\Controller\V1;
 
 
 use App\ApiBundle\Controller\AbstractApiController;
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,64 +23,115 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractApiController
 {
     /**
-     * @Route ("/")
-     * @param Request $request
+     * @Route ("/", methods={"GET"})
      * @return Response
      */
-    public function indexAction(Request $request): Response
+    public function indexAction(): Response
     {
-        return $this->respond("");
+        /** @var ContactRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(Contact::class);
+        /** @var Contact[] $allContacts */
+        $allContacts = $repository->findAll();
+
+        return $this->respond($allContacts);
     }
 
     /**
-     * @Route ("/")
+     * @Route ("/", methods={"POST"})
      * @param Request $request
      * @return Response
      */
     public function createAction(Request $request): Response
     {
+        /** @var ContactRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(Contact::class);
+
         $form = $this->buildForm(Contact::class);
         $form->handleRequest($request);
 
-
         if (!$form->isSubmitted() || !$form->isValid()) {
-            $this->respond($form, Response::HTTP_BAD_REQUEST);
+            return $this->respond($form, Response::HTTP_BAD_REQUEST);
         }
 
         /** @var Contact $contact */
         $contact = $form->getData();
 
+        $repository->saveContact($contact);
+
         return $this->respond($contact);
     }
 
     /**
-     * @Route ("/{id}")
-     * @param Request $request
+     * @Route ("/{id}", methods={"GET"})
+     * @param int $id
      * @return Response
      */
-    public function showAction(Request $request): Response
+    public function showAction($id): Response
     {
-        return $this->respond("");
+        /** @var ContactRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(Contact::class);
+        /** @var Contact $contact */
+        $contact = $repository->findOneBy(['id' => $id]);
+
+        if (null === $contact) {
+            return $this->respond($contact, Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this->respond($contact);
     }
 
     /**
-     * @Route ("/{id}")
+     * @Route ("/{id}", methods={"PATCH, PUT"})
+     * @param int $id
      * @param Request $request
      * @return Response
      */
-    public function updateAction(Request $request): Response
+    public function updateAction($id, Request $request): Response
     {
-        return $this->respond("");
+        /** @var ContactRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(Contact::class);
+        /** @var Contact $contact */
+        $contact = $repository->findOneBy(['id' => $id]);
+
+        $form = $this->buildForm(Contact::class);
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->respond($form, Response::HTTP_BAD_REQUEST);
+        }
+
+        $validContact = $form->getData();
+
+        if (!empty($validContact['email'])) {
+            $contact->setEmail($validContact['email']);
+        }
+        if (!empty($validContact['fname'])) {
+            $contact->setFname($validContact['fname']);
+        }
+        if (!empty($validContact['lname'])) {
+            $contact->setLname($validContact['lname']);
+        }
+
+        $repository->saveContact($contact);
+
+        return $this->respond($contact);
     }
 
     /**
-     * @Route ("/{id}")
-     * @param Request $request
+     * @Route ("/{id}", methods={"DELETE"})
+     * @param int $id
      * @return Response
      */
-    public function deleteAction(Request $request): Response
+    public function deleteAction($id): Response
     {
-        return $this->respond(null);
+        /** @var ContactRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(Contact::class);
+        /** @var Contact $contact */
+        $contact = $repository->findOneBy(['id' => $id]);
+
+        $repository->deleteContact($contact);
+
+        return $this->respond(null, Response::HTTP_NO_CONTENT);
     }
 
 }
--- src/Form/Type/ContactType.php
+++ src/Form/Type/ContactType.php
@@ -8,11 +8,14 @@
 
 namespace App\Form\Type;
 
+use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotNull;
 
 class ContactType extends AbstractType
@@ -20,6 +23,20 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('fname', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 255
+                    ])
+                ]
+            ])
+            ->add('lname', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 255
+                    ])
+                ]
+            ])
             ->add('email', EmailType::class, [
                 'constraints' => [
                     new NotNull(['message' => 'Email should not be blank.']),
